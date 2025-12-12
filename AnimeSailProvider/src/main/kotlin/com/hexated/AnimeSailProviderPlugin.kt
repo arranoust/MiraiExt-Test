99dd346034ffcf7687590ed3cbd0ff4f6c8529ba
arranoust <arranoust@gmail.com>
2025-12-12 19:35:49 +0700

    fix

--- AnimeSailProvider/src/main/kotlin/com/hexated/AnimeSailProviderPlugin.kt
+++ AnimeSailProvider/src/main/kotlin/com/hexated/AnimeSailProviderPlugin.kt
@@ -1,14 +1,11 @@
-
 package com.hexated
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class AnimeSailProviderPlugin: Plugin() {
-    override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
+class AnimeSailProviderPlugin : Plugin() {
+    override fun load(context: android.content.Context) {
         registerMainAPI(AnimeSailProvider())
     }
 }
